3d6d0b5d69c1e27df5193f6a4f614c1264b6287b
Aditya Mali <152274255+AdityaNow@users.noreply.github.com>
2024-09-20 22:41:50 +0530

    update-var (#2)

--- Calculator/addition.js
+++ Calculator/addition.js
@@ -4,8 +4,8 @@ function addTwoNumbers(a, b) {
   }
   
   // Example usage
-  let num1 = 5;
+  let num1 = 12;
   let num2 = 10;
   
   let sum = addTwoNumbers(num1, num2);
-  console.log("The sum is: " + sum);  
+  console.log("The sum is: " + sum);
